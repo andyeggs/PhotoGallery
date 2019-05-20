fd0cede2da6218e4d77cdf2e01bd36c28ce33990
Andy E <andyjohneggs@gmail.com>
2019-05-20 08:23:32 -0500

    Initial commit!

---
+++ app/src/main/java/com/ctech/andyeggs/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package com.ctech.andyeggs.photogallery;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
