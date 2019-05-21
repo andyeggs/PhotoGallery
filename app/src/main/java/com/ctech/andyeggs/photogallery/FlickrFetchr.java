feda642914f7ddd4e76624761d62667ead0335ad
Andy E <andyjohneggs@gmail.com>
2019-05-21 08:53:14 -0500

    Unit 25, Slightly past halfway, CREATE A FALLERYITEM

--- app/src/main/java/com/ctech/andyeggs/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/ctech/andyeggs/photogallery/FlickrFetchr.java
@@ -1,12 +1,37 @@
 package com.ctech.andyeggs.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 
 public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "907caa6a96a49d83fb68d47f48b60a76";
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Recieved JSON: " + jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+    }
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
